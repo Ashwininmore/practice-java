80828fce506a0e935741893c9b2bc8b280afcfe1
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-02-20 17:49:05 +0530

    scanner-class

--- src/basic/ScanData.java
+++ src/basic/ScanData.java
@@ -1,10 +1,21 @@
+//read data using scanner
 package basic;
+
+import java.util.Scanner;
 
 public class ScanData {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter name:");
+		String nm=sc.next();
+		System.out.println("Enter Age:");
+		int age=sc.nextInt();
+		System.out.println("Enter Profession:");
+		String pf=sc.next();
+		
+		System.out.println("| Name | Age | Profession |");
+		System.out.println("| "+nm+" | "+age+" | "+pf+" |");
 	}
 
 }
