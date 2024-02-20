100f6ac77bd2dd3e0b500c5a2304b1eec396219d
Ashwini Nitin More <69009147+Ashwininmore@users.noreply.github.com>
2024-02-20 17:56:39 +0530

    //Addition of n numbers

---
+++ src/basic/Prb1.java
@@ -0,0 +1,20 @@
+//Addition of n numbers
+package basic;
+
+import java.util.Scanner;
+
+public class Prb1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n:");
+		int n=sc.nextInt();
+		int i=1, sum=0;
+		while(i<=n) {
+			sum=sum+i;
+			i++;
+		}
+		System.out.println("Addition of "+n+" numbers :"+sum);
+	}
+
+}
